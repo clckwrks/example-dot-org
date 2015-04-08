ebff137be6ea4936cbc4651e9893c812bbf1d29a
Jeremy Shaw <jeremy@n-heptane.com>
2015-04-08 11:48:41 -0500

    remove depends on hsx2hs preprocessor.

--- Setup.hs
+++ Setup.hs
@@ -5,9 +5,6 @@ module Main where
 import Distribution.Simple
 import Distribution.Simple.Program
 
-hsx2hsProgram = simpleProgram "hsx2hs"
-
 main :: IO ()
-main = defaultMainWithHooks simpleUserHooks {
-         hookedPrograms = [hsx2hsProgram]
-       }
+main = defaultMainWithHooks simpleUserHooks
+
