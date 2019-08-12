edaeaa0859dc76cc6b9228a1c1b7d19063f7cdca
Jeremy Shaw <jeremy@n-heptane.com>
2019-08-11 21:33:56 -0500

    allow newer build depends

--- shell.nix
+++ shell.nix
@@ -1,8 +1,8 @@
 with (import <nixpkgs> {}).pkgs;
-let pkg = haskellngPackages.callPackage
+let pkg = haskellPackages.callPackage
             ({ mkDerivation, base, clckwrks, clckwrks-plugin-page
              , clckwrks-theme-bootstrap, containers, happstack-server, hsp
-             , hsx2hs, markdown, mtl, stdenv, text, web-plugins, pandoc
+             , hsx2hs, markdown, mtl, stdenv, text, web-plugins, pandoc, cabal-install
              }:
              mkDerivation {
                pname = "example-dot-org";
@@ -14,7 +14,7 @@ let pkg = haskellngPackages.callPackage
                  base clckwrks clckwrks-plugin-page clckwrks-theme-bootstrap
                  containers happstack-server hsp mtl text web-plugins
                ];
-               buildTools = [ hsx2hs pandoc ];
+               buildTools = [ hsx2hs pandoc  cabal-install ];
                homepage = "http://www.clckwrks.com/";
                description = "an example clckwrks site";
                license = stdenv.lib.licenses.bsd3;
